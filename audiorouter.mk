4478ead1bb8114f41a61603f43a6147af368f8b3
Oil Can Steve <oilcansteve@hotmail.com>
2026-01-23 18:37:09 +0000

    info better expressed

--- audiorouter.mk
+++ audiorouter.mk
@@ -3,7 +3,7 @@
 # audiorouter
 #
 ######################################
-AUDIOROUTER_VERSION = f5814698c669e6905f7528fb3ebaa71c7dd9a70c
+AUDIOROUTER_VERSION = 199e61890ef21011b42824c54b10c39952a9a926
 AUDIOROUTER_SITE = https://github.com/stivb/audiorouter.git
 AUDIOROUTER_SITE_METHOD = git
 AUDIOROUTER_BUNDLES = audiorouter.lv2
