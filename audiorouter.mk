f2526b5748e4ab7e81cb3eeade2b6a3410878c08
Oil Can Steve <oilcansteve@hotmail.com>
2026-01-23 10:45:19 +0000

    rearranged stuff

---
+++ audiorouter.mk
@@ -0,0 +1,21 @@
+######################################
+#
+# audiorouter
+#
+######################################
+AUDIOROUTER_VERSION = 3419e183eb5be1cd0b7e54ee90238a97e0d8ce52
+AUDIOROUTER_SITE = https://github.com/stivb/audiorouter.git
+AUDIOROUTER_SITE_METHOD = git
+AUDIOROUTER_BUNDLES = audiorouter.lv2
+
+AUDIOROUTER_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) MOD=1 OPTIMIZATIONS="" PREFIX=/usr -C $(@D)/audiorouter/source
+
+define AUDIOROUTER_BUILD_CMDS
+	$(AUDIOROUTER_TARGET_MAKE)
+endef
+
+define AUDIOROUTER_INSTALL_TARGET_CMDS
+	$(AUDIOROUTER_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
